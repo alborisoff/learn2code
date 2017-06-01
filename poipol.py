ec0d214a7ba3d949664d056173bfab49509cdf53
Aleksey Borisov <alekseyaborisoff@gmail.com>
2017-06-01 17:14:00 +0300

    Update poipol.py
    Добавил функцию, вычисляющую координаты пересечения двух прямых, каждая из которых проходит через две точки. Координаты пар точек являются входными данными.

--- poipol.py
+++ poipol.py
@@ -22,6 +22,31 @@ def  gabarits(arrofpoints):
                 maxy = arrofpoints[j][1]
     return minx, maxx, miny, maxy
 
+def intersect(segment1, segment2):  # Координаты пересечения двух прямых, проходящих через две точки каждая
+
+    x11 = segment1[0][0]
+    y11 = segment1[0][1]
+    x12 = segment1[1][0]
+    y12 = segment1[1][1]
+
+    x21 = segment2[0][0]
+    y21 = segment2[0][1]
+    x22 = segment2[1][0]
+    y22 = segment2[1][1]
+
+    a1 = y11 - y12
+    b1 = x12 - x11
+    c1 = x11*y12 - x12*y11
+
+    a2 = y21 - y22
+    b2 = x22 - x21
+    c2 = x21*y22 - x22*y21
+
+    y = (a2*c1 - a1*c2) / (a1*b2 - a2*b1)
+    x = (-c1 - b1*y) / a1
+
+    return x, y
+
 arra = [[0, 0],
         [10, 2],
         [1, 1],
